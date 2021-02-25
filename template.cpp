adb1b5efffb5b64c293f97849cca48dd054797ff
Venkatakrishnan Asuri <76925333+abcvenkatxyz@users.noreply.github.com>
2021-02-26 00:58:18 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long int
+#define vi vector<ll>
+#define pii pair<ll,ll>
+#define fastio ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define pb push_vack
+#define mp make_pair
+#define f first
+#define s second
+#define rep(i,a,b) for (ll i = a; i <= b; i++)
+#define repn(i,a,b) for (ll i = a; i < b; i++)
+#define urep(i,a,b) for (ull i = a; i <= b; i++)
+#define urepn(i,a,b) for (ull i = a; i < b; i++)
+#define maxx(a,b,c) max(max(a,b),c)
+#define minn(a,b,c) min(min(a,b),c)
+#define INF 1e9
+#define MOD 1000000007
+/*
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+*/
+
+int main(){
+    fastio;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout); 
+    #endif
+    
+}
+
